001f0700d6ecfb9ec8ac3c7d93c2c90b8219d6ef
Israel Araujo <rodg.isra@gmail.com>
2020-03-30 04:11:08 -0300

    header modificado para ações do userdropdown

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app" :class="{'hide-menu': !isMenuVisible}">
+    <Header title="Cod3r - Base de Conhecimento" :hideToggle="false" :hideUserDropdown="false"/>
+    <Menu />
+    <Content />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import Header from "./components/template/Header.vue";
+import Menu from "./components/template/Menu.vue";
+import Content from "./components/template/Content.vue";
+import Footer from "./components/template/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Menu,
+    Content,
+    Footer
+  },
+  computed: mapState(['isMenuVisible'])
+};
+</script>
+
+<style>
+* {
+  font-family: "Lato", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  height: 100vh;
+  display: grid;
+  grid-auto-rows: 50px 1fr 40px;
+  grid-template-columns: 250px 1fr;
+  grid-template-areas:
+    "header header"
+    "menu content"
+    "menu footer";
+}
+
+#app.hide-menu {
+  grid-template-areas: 
+  "header header"
+  "content content"
+  "footer footer";
+}
+</style>
---
+++ src/components/template/Content.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="content"></div>
+</template>
+
+<script>
+export default {
+  name: "Content"
+};
+</script>
+
+<style>
+.content {
+  grid-area: content;
+  background-color: #eeffee;
+  padding: 20px;
+}
+</style>
